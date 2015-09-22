9ccc00e64144590494e6460ff07612cb997555c5
picflute <picflute@users.noreply.github.com>
2015-09-21 22:54:24 -0400

    did more work

--- Sinusoids.m
+++ Sinusoids.m
@@ -1,7 +1,7 @@
 t =0:.25:8;
 x1 = exp(-.5 * t);
 x2 = exp(-3.0 * t);
-%plot(t,x1,'g',t,x2,'b');
+plot(t,x1,'g',t,x2,'b');
 xlabel('Time');
 ylabel('Value');
     
@@ -9,9 +9,19 @@ ylabel('Value');
 subplot(311);
 t2 = 0:.25:5;
 p1 = sin(0.5 * pi * t2);
-plot(p1,t2);
+plot(t2,p1);
+xlabel('Time');
+ylabel('Sin Value');
+title('w = 0.5pi Theta = 0');
 subplot(312);
 p2 = sin(1.2 * pi * t2);
-plot(p2,t2);
+plot(t2,p2);
+xlabel('Time');
+ylabel('Sin Value');
+title('w = 1.2pi Theta = 0');
 subplot(313);
-p3= sin(
+p3 = sin(1.2 * pi * t2);
+plot(t2,p3);
+xlabel('Time');
+ylabel('Sin Value');
+title('w = 1.2 Theta = pi');
